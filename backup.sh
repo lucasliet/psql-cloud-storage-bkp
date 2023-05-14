3388d201a4026b0571875d6bf8ba25b0dd461156
Lucas Oliveira <lucasouliveira@gmail.com>
2023-05-13 23:06:33 -0300

    Show backup file size alongside time on completion

--- backup.sh
+++ backup.sh
@@ -16,14 +16,15 @@ echo "Starting $DATABASE_NAME backup"
 
 CURRENT_DATE=$(date +%F)
 TABLES=($(psql -h $DATABASE_URL -U $DATABASE_USER -w -d $DATABASE_NAME -c "\dt" | sed -n 's/^[^|]*| *\([^|]*\)|.*$/\1/p' | grep -v Name | xargs))
+SCHEMA_FILE_NAME_PATH=gs://$BACKUP_BUCKET/$DATABASE_NAME-dump/$DATABASE_NAME-schema.sql.gz
 
 echo "Tables to backup: ${TABLES[@]}"
 
 pg_dump -h $DATABASE_URL -U $DATABASE_USER -w --schema-only $DATABASE_NAME \
 | gzip | gsutil -q cp -z gzip \
-- gs://$BACKUP_BUCKET/$DATABASE_NAME-dump/$DATABASE_NAME-schema.sql.gz
+- $SCHEMA_FILE_NAME_PATH
 
-echo "$DATABASE_NAME schema has been backed up to gs://$BACKUP_BUCKET/$DATABASE_NAME-dump/$DATABASE_NAME-schema.sql.gz"
+echo "$DATABASE_NAME schema has been backed up to $SCHEMA_FILE_NAME_PATH"
 echo
 
 for TABLE in ${TABLES[@]}; do
@@ -32,10 +33,10 @@ for TABLE in ${TABLES[@]}; do
     START_TIME=$(date +%s)
     CURRENT_MONTH=$(date -d "$CURRENT_DATE" +%Y-%m)-01
     LAST_MONTH=$(date -d "$CURRENT_DATE -1 month" +%Y-%m)-01
-    FILE_NAME=$TABLE-$(date -d "$CURRENT_DATE -1 month" +%Y-%m).csv.gz
+    BACKUP_FILE_PATH=gs://$BACKUP_BUCKET/$DATABASE_NAME-dump/tables/$TABLE/$TABLE-$(date -d "$CURRENT_DATE -1 month" +%Y-%m).csv.gz
 
-    if gsutil -q stat gs://$BACKUP_BUCKET/$DATABASE_NAME-dump/tables/$TABLE/$FILE_NAME; then
-      echo "File gs://$BACKUP_BUCKET/$DATABASE_NAME-dump/tables/$TABLE/$FILE_NAME already exists, skipping backup"
+    if gsutil -q stat $BACKUP_FILE_PATH; then
+      echo "File $BACKUP_FILE_PATH already exists, skipping backup"
       echo
       CURRENT_DATE=$(date -d "$CURRENT_DATE -1 month" +%F)
       continue
@@ -43,14 +44,16 @@ for TABLE in ${TABLES[@]}; do
 
     echo "Backing up $LAST_MONTH of table $TABLE"
     echo "WHERE created_at >= '$LAST_MONTH' AND created_at < '$CURRENT_MONTH'"
-    echo "Destination file gs://$BACKUP_BUCKET/$DATABASE_NAME-dump/tables/$TABLE/$FILE_NAME"
+    echo "Destination file $BACKUP_FILE_PATH"
     
     psql -h $DATABASE_URL -U $DATABASE_USER -w -d $DATABASE_NAME \
     -c "COPY (SELECT * FROM $TABLE WHERE created_at >= '$LAST_MONTH' AND created_at < '$CURRENT_MONTH') TO STDOUT WITH (DELIMITER ';', FORMAT CSV);" \
     | gzip | gsutil -q cp -z gzip -s archive \
-    - gs://$BACKUP_BUCKET/$DATABASE_NAME-dump/tables/$TABLE/$FILE_NAME
+    - $BACKUP_FILE_PATH
 
-    echo "$FILE_NAME Backup took $(($(date +%s) - $START_TIME)) seconds"
+    BACKUP_FILE_SIZE=$(gsutil du $BACKUP_FILE_PATH | awk '{print $1}')
+
+    echo "Backup took $(($(date +%s) - $START_TIME)) seconds with $BACKUP_FILE_SIZE bytes"
     echo
 
     CURRENT_DATE=$(date -d "$CURRENT_DATE -1 month" +%F)
